7fa941ee5c253a142f5933b3cd31657ec2445fe4
Andy Shora <andy.shora@qb0003-mac-2.local>
2017-03-10 09:26:29 +0000

    Lint fix

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,9 @@ import './styles/index.css';
 const React = require('react');
 const ReactDOM = require('react-dom');
 
+/**
+ * Our main application
+ */
 const App = () => <div>Hello World</div>;
 
 ReactDOM.render(<App />, document.getElementById('root'));
